8716e8f55f90ef6a42c49be3721109680ed01cba
rakibur557 <rakiburrahman.cse.eng@gmail.com>
2024-12-11 22:52:30 +0600

    https://leetcode.com/problems/length-of-last-word

---
+++ length_of_last_word.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        # Strip trailing spaces and split the string into words
+        words = s.strip().split()
+        # Return the length of the last word
+        return len(words[-1])
